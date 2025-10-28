1d2ed628c5ea44b1140644ccda7e04a0cb730700
cch <choechanghyeon@MacBook-Pro.local>
2025-10-28 22:21:08 +0900

    Post 요청 만들기 부터 진행

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
 /**
@@ -9,28 +9,74 @@ import { PostsService } from './posts.service';
  * commentCount: number;
  */
 
-interface Post {
+interface PostModal {
+  id: number;
   author: string;
   title: string;
   content: string;
   likeCount: number;
   commentCount: number;
 }
+
+let posts: PostModal[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 10000000,
+    commentCount: 9999999,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스 해린',
+    content: '노래 연습 하고 있는 해린',
+    likeCount: 10000000,
+    commentCount: 9999999,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '블랙핑크 로제',
+    content: '종합운동장에서 공연중인 로제',
+    likeCount: 10000000,
+    commentCount: 9999999,
+  },
+];
+
 // 1차 path 주소 /post
 // controller 클래스에 있는 "모든" 라우터 앞에 붙는 접두어.
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
-
-  // 2차 path 주소
+  // 1) GET /posts
+  // 모든 post를 다 가져온다
   @Get()
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: '뉴진스 민지',
-      content: '메이크업 고치고 있는 민지',
-      likeCount: 10000000,
-      commentCount: 9999999,
-    };
+  getPosts() {
+    return posts;
   }
+
+  // 2) GET /posts/:id
+  // id에 해당되는 post를 가져온다
+  // 예를 들어서 id=1일경우 id가 1인 포스트를 가져온다.
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    return post;
+  }
+
+  // 3) POST /posts
+  //    POST를 생성한다.
+
+  // 4) PUT /posts/:id
+  //    id에 해당되는 POST를 변경한다.
+
+  // 5) DELETE /posts/:id
+  //    id에 해당되는 POST를 삭제한다.
 }
